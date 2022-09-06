135d617f28220f4320418650e5055ca2b88180b0
Jaydev Singh Rao <jaydevsrao@gmail.com>
2022-09-06 16:24:51 +0530

    Create q1.py

---
+++ Assignment1/q1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Line between two points in standard form: Ax + By + C = 0
+class Line:
+    self.A = 0
+    self.B = 0
+    self.C = 0
+    def __init__(self, p1, p2):
+        # set values of self.A, self.B, self.C
+        
+    def slope(self):
+        return (-self.A / self.B)
+
+# 
