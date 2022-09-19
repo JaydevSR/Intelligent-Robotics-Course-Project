4f29423af6e5cd7c1ebef89ab1b82a045ce326f6
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-09-20 00:00:22 +0530

    add visibility graph

---
+++ MidSem/visibility_graph.py
@@ -0,0 +1,33 @@
+from sympy import Polygon, Point, Line, Segment2D, Point2D, Line2D
+
+# visibility graph of multiple polygons
+def visibility_graph(start, goal, polygons):
+    points = [start, goal]
+    for p in polygons:
+        points.extend(p.vertices)
+    points = list(set(points)) # remove duplicates
+    print(points)
+    points.sort(key=lambda v: v.args[0]) # sort by x
+
+    edges = []
+    for p in polygons:
+        edges.extend(p.sides)
+
+    visgraph = []
+
+    for p in range(len(points)-1):
+        for q in range(p+1, len(points)):
+            line = Line(points[p], points[q])
+            flag = True
+            for e in edges:
+                isect = e.intersect(line)
+                if isect.is_empty:
+                    continue
+                elif type(isect) is Segment2D:
+                    continue
+                elif not list(isect)[0] in e.args:
+                    flag = False
+                    break
+            if flag:
+                visgraph.append((points[p], points[q]))
+    return set(visgraph)
