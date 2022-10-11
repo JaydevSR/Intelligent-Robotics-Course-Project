02c6d9d5404de9782a1fc226dbb68fead1014525
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-10-12 00:16:04 +0530

    update visibility graph env

--- MidSem/visibility_graph.py
+++ MidSem/visibility_graph.py
@@ -56,15 +56,6 @@ if __name__ == "__main__":
         ax3.add_patch(plt.Polygon(p.vertices, closed=True, fill=True))
         ax4.add_patch(plt.Polygon(p.vertices, closed=True, fill=True))
 
-    # plot start and goal
-    for ax in [ax1, ax2, ax3, ax4]:
-        ax.axis('equal')
-        ax.plot(start.args[0], start.args[1], 'ro', label='start')
-        ax.plot(goal.args[0], goal.args[1], 'go', label='goal')
-        # set limits
-        ax.set_xlim(-1, 11)
-        ax.set_ylim(-1, 11)
-
     visgraph = visibility_graph(start, goal, polygons)
     # print("Visibility graph:")
     # for e in visgraph:
@@ -73,28 +64,50 @@ if __name__ == "__main__":
     # plot visibility graph
     for e in visgraph:
         ax2.plot([e[0].args[0], e[1].args[0]], [e[0].args[1], e[1].args[1]], 'b-')
-        ax4.plot([e[0].args[0], e[1].args[0]], [e[0].args[1], e[1].args[1]], 'b-')
     ax2.plot([], [], 'b-', label="Visibility Graph")
-    ax4.plot([], [], 'b-', label="Visibility Graph")
 
     adj_list, weights = get_adjacency_list(visgraph)
     path = a_star(start, goal, adj_list, weights)
     # plot shortest path
     for i in range(len(path)-1):
-        ax3.plot([path[i].args[0], path[i+1].args[0]], [path[i].args[1], path[i+1].args[1]], 'r-')
         ax4.plot([path[i].args[0], path[i+1].args[0]], [path[i].args[1], path[i+1].args[1]], 'r-')
-    ax3.plot([], [], 'r-', label="Shortest Path")
     ax4.plot([], [], 'r-', label="Shortest Path")
-
     # print("Shortest path:")
     # print(path)
+
+    # Bug 0 path
+    bug0_path = [
+        start,
+        Point2D(17/10, 1),
+        Point(0, 1),
+        Point(0, 2.5),
+        Point2D(4, 25/4),
+        Point2D(4, 7),
+        goal
+    ]
+
+    # plot Bug 0 path
+    for i in range(len(bug0_path)-1):
+        ax3.plot([bug0_path[i].args[0], bug0_path[i+1].args[0]], [bug0_path[i].args[1], bug0_path[i+1].args[1]], 'g-')
+        ax4.plot([bug0_path[i].args[0], bug0_path[i+1].args[0]], [bug0_path[i].args[1], bug0_path[i+1].args[1]], 'g-')
+    ax3.plot([], [], 'g-', label="Bug 0 Path")
+    ax4.plot([], [], 'g-', label="Bug 0 Path")
+
+    # plot start and goal
+    for ax in [ax1, ax2, ax3, ax4]:
+        ax.axis('equal')
+        ax.plot(start.args[0], start.args[1], 'ro', label='start', markersize=10)
+        ax.plot(goal.args[0], goal.args[1], 'go', label='goal', markersize=10)
+        # set limits
+        ax.set_xlim(-1, 11)
+        ax.set_ylim(-1, 11)
 
     # legend
     for ax in [ax2, ax3, ax4]:
         ax.legend()
 
     # plot title
-    plt.title("Visibility graph and shortest path")
+    plt.title("Visibility graph, Shortest path and Bug 0 path")
 
     # save plot
     plt.savefig("MidSem/plots/visibility_graph.png")
